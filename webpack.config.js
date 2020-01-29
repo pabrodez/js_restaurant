50da2492b5fab5f5e1f5c5714aa0052ee9e03e4d
Ubuntu <ubuntu@ip-172-31-33-120.eu-west-1.compute.internal>
2020-01-29 20:03:01 +0000

    change webpack config to simple sass pre-load rules, tweak menus and dimensions

--- webpack.config.js
+++ webpack.config.js
@@ -13,10 +13,8 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: [
-          // fallback to style-loader in development
-          process.env.NODE_ENV !== 'production'
-            ? 'style-loader'
-            : MiniCssExtractPlugin.loader,
+          // Creates `style` nodes from JS strings
+          'style-loader',
           // Translates CSS into CommonJS
           'css-loader',
           // Compiles Sass to CSS
